837d2e1e38b749bd49261a59f14a26033ab8003c
Wuiz Zamora <wuizamora21@gmail.com>
2023-11-17 23:49:05 -0600

    SE AGREGO HEADER Y FOOTER, FUNCIONAMIENTO DE CLIENTE OK

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const xml2js = require('xml2js');
 const multer  = require('multer');
 const upload = multer();
 
-const port = 5000; // Definir el puerto que quieres utilizar
+const port = 5000; // Define el puerto que se utiliza
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,7 +38,19 @@ app.get('/consulta', (req, res) => {
 
         res.send(`<h1>Resultados de la consulta SELECT:</h1> 
         <pre>${JSON.stringify(rows, null, 4)}</pre>`);
-    });
+    }); 
+});
+
+app.get('/consultaCliente', (req, res) => {
+db.all('SELECT * FROM Cliente', [], (err, rows) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Resultados de la consulta SELECT: ', rows);
+
+    res.send(`<h1>Resultados de la consulta SELECT para Clientes:</h1> 
+    <pre>${JSON.stringify(rows, null, 4)}</pre>`);
+});
 });
 
 app.post('/login', (req, res) => {
@@ -70,47 +82,139 @@ app.post('/upload', upload.single('xml'), (req, res) => {
         } else {
             const comprobante = result['cfdi:Comprobante'];
             console.log('Datos extraídos del archivo XML:');
-            console.log('Serie:', comprobante.$.serie);
-            console.log('Folio:', comprobante.$.folio);
-            console.log('Fecha:', comprobante.$.fecha);
-            console.log('Subtotal:', comprobante.$.subTotal);
-            console.log('Descuento:', comprobante.$.descuento);
-            console.log('Total:', comprobante.$.total);
-            console.log('Tipo de Comprobante:', comprobante.$.tipoDeComprobante);
+            console.log('Folio:', comprobante.$.Folio);
+            console.log('Fecha:', comprobante.$.Fecha);
+            console.log('Subtotal:', comprobante.$.SubTotal);
+            console.log('Total:', comprobante.$.Total);
+            console.log('Tipo de Comprobante:', comprobante.$.TipoDeComprobante);
 
             const emisor = comprobante['cfdi:Emisor'];
-            console.log('Emisor RFC:', emisor.$.rfc);
-            console.log('Emisor Nombre:', emisor.$.nombre);
+            console.log('Emisor RFC:', emisor.$.Rfc);
+            console.log('Emisor Nombre:', emisor.$.Nombre);
 
             const receptor = comprobante['cfdi:Receptor'];
-            console.log('Receptor RFC:', receptor.$.rfc);
-            console.log('Receptor Nombre:', receptor.$.nombre);
+            console.log('Receptor RFC:', receptor.$.Rfc);
+            console.log('Receptor Nombre:', receptor.$.Nombre);
 
             const conceptos = comprobante['cfdi:Conceptos']['cfdi:Concepto'];
-            console.log('Conceptos:');
-            conceptos.forEach((concepto, index) => {
-            console.log(`Concepto ${index + 1}:`);
-            console.log('  Cantidad:', concepto.$.cantidad);
-            console.log('  Unidad:', concepto.$.unidad);
-            console.log('  No. Identificación:', concepto.$.noIdentificacion);
-            console.log('  Descripción:', concepto.$.descripcion);
-            console.log('  Valor Unitario:', concepto.$.valorUnitario);
-            console.log('  Importe:', concepto.$.importe);
-            });
 
-            const impuestosLocales = comprobante['cfdi:Complemento']['implocal:ImpuestosLocales'];
-            console.log('Impuestos Locales:');
-            console.log('  Total de Retenciones:', impuestosLocales.$.TotaldeRetenciones);
-            console.log('  Total de Traslados:', impuestosLocales.$.TotaldeTraslados);
+            console.log('Conceptos:');
+            if (Array.isArray(conceptos)) {
+                conceptos.forEach((concepto, index) => {
+                    console.log(`Concepto ${index + 1}:`);
+                    console.log('  Cantidad:', concepto.$.Cantidad);
+                    console.log('  Unidad:', concepto.$.Unidad);
+                    console.log('  No. Identificación:', concepto.$.NoIdentificacion);
+                    console.log('  Descripción:', concepto.$.Descripcion);
+                    console.log('  Valor Unitario:', concepto.$.ValorUnitario);
+                    console.log('  Importe:', concepto.$.Importe);
+
+                    // Impuestos
+                    if (concepto['cfdi:Impuestos'] && concepto['cfdi:Impuestos']['cfdi:Traslados']) {
+                        const traslados = concepto['cfdi:Impuestos']['cfdi:Traslados']['cfdi:Traslado'];
+                        console.log('  Impuestos Trasladados:');
+                        traslados.forEach((traslado, tIndex) => {
+                            console.log(`    Traslado ${tIndex + 1}:`);
+                            console.log('      Impuesto:', traslado.$.Impuesto);
+                            console.log('      Tasa o Cuota:', traslado.$.TasaOCuota);
+                            console.log('      Importe:', traslado.$.Importe);
+                        });
+                    }
+                });
+            } else {
+                // Si no es un array, procesar un solo concepto
+                console.log(`Concepto único:`);
+                console.log('  Cantidad:', conceptos.$.Cantidad);
+                console.log('  Unidad:', conceptos.$.Unidad);
+                console.log('  No. Identificación:', conceptos.$.NoIdentificacion);
+                console.log('  Descripción:', conceptos.$.Descripcion);
+                console.log('  Valor Unitario:', conceptos.$.ValorUnitario);
+                console.log('  Importe:', conceptos.$.Importe);
+                // Puedes agregar el código para impuestos aquí si es necesario
+            }
+
+            // Impuestos Globales
+           // Impuestos Globales
+const impuestos = comprobante['cfdi:Impuestos'];
+if (impuestos && impuestos['cfdi:Traslados']) {
+    const trasladosGlobales = impuestos['cfdi:Traslados']['cfdi:Traslado'];
+
+    console.log('Impuestos Globales Trasladados:');
+    if (Array.isArray(trasladosGlobales)) {
+        trasladosGlobales.forEach((trasladoGlobal, tgIndex) => {
+            console.log(`  Traslado Global ${tgIndex + 1}:`);
+            console.log('    Impuesto:', trasladoGlobal.$.Impuesto);
+            console.log('    Tasa o Cuota:', trasladoGlobal.$.TasaOCuota);
+            console.log('    Importe:', trasladoGlobal.$.Importe);
+        });
+    } else {
+        // Si no es un array, procesar un solo traslado global
+        console.log(`  Traslado Global único:`);
+        console.log('    Impuesto:', trasladosGlobales.$.Impuesto);
+        console.log('    Tasa o Cuota:', trasladosGlobales.$.TasaOCuota);
+        console.log('    Importe:', trasladosGlobales.$.Importe);
+    }
+}
+
+            // Timbre Fiscal Digital
+            const timbreFiscalDigital = comprobante['cfdi:Complemento']['tfd:TimbreFiscalDigital'];
+            console.log('Timbre Fiscal Digital:');
+            console.log('  UUID:', timbreFiscalDigital.$.UUID);
+            console.log('  Fecha de Timbrado:', timbreFiscalDigital.$.FechaTimbrado);
+            console.log('  Sello SAT:', timbreFiscalDigital.$.SelloSAT);
+            console.log('----------------------------------------------------------------------------');
             res.status(200).send('Los datos se han procesado con éxito. Revise la terminal del servidor para ver los resultados.');
         }
     });
 });
 
-
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
+const sql_create="CREATE TABLE IF NOT EXISTS Cliente(nombre VARCHAR(50) NOT NULL,rfc  VARCHAR(13) NOT NULL PRIMARY KEY)"
+db.run(sql_create, err =>{
+        if(err){
+            return console.error(err.message);
+        }else{
+            console.log("TABLA CREADA EXITOSAMENTE");
+        }
+    })
+
+    app.post('/insertarCliente', upload.single('xml'), (req, res) => {
+        const xml = req.file.buffer.toString();
+        const parser = new xml2js.Parser({ explicitArray: false });
+    
+        parser.parseString(xml, (err, result) => {
+            if (err) {
+                console.error('Error al analizar el XML:', err);
+                return res.status(500).json({ message: 'Error al analizar el XML' });
+            } else {
+                const receptor = result['cfdi:Comprobante']['cfdi:Receptor'];
+
+                const receptorRFC = receptor.$.Rfc;
+                const receptorNombre = receptor.$.Nombre;
+
+                console.log('DATOS DEL CLIENTE:');
+                console.log('Receptor RFC:', receptorRFC);
+                console.log('Receptor Nombre:', receptorNombre);
+                console.log('-------------------------------------------');
+                db.run('INSERT INTO Cliente (nombre, rfc) VALUES (?, ?)', [receptorNombre, receptorRFC], function(err) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                    console.log(`Se ha insertado un nuevo Cliente en la Base de datos`);
+                });
+            res.status(200).json({
+                receptorRFC: receptorRFC,
+                receptorNombre: receptorNombre
+            });
+            }
+        });
+    });
+
+    
+
 
 // db.get('SELECT * FROM Usuarios WHERE usuario = ?', [usuario], (err, row) => {
     //     if (err) {
